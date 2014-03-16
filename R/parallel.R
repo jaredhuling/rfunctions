af21451460f7f511499e7c4e60ef48267d7d9228
jaredhuling <huling@wisc.edu>
2014-03-16 13:43:48 -0500

    added comments to runParallel()

--- R/parallel.R
+++ R/parallel.R
@@ -1,7 +1,18 @@
 
-
-runParallel <- function(ncores = NULL){
+#' Initialized parallel workers
+#'
+#' @param ncores integer number of cores to initialize
+#' @return cluster object 
+#' @export
+#' @examples
+#'cl <- runParallel(ncores = 4L)
+#'x <- 0
+#'foreach(i=1:100) %dopar$ {
+#'x <- x + i ^ 2
+#'}
+runParallel <- function(ncores = 1L){
   require(parallel)
+  ncores <- as.integer(ncores)
   switch(Sys.info()[["sysname"]], 
          "Windows" = {
            library(doSNOW)
