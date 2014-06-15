19093ad789e3c7762fd5470585439df5dcd6b0ca
jaredhuling <huling@wisc.edu>
2014-06-15 14:44:32 -0500

    added docs

--- R/boost_funcs.R
+++ R/boost_funcs.R
@@ -1,5 +1,18 @@
 
-
+#'  distribution function
+#'
+#' @param q vector of quantiles
+#' @param mean scalar mean (vector will be implemented)
+#' @param sd scalar standard deviation (vector will be implemented)
+#' @param lower.tail logical; if TRUE (default), probabilities are P[X <= x] otherwise, P[X > x].
+#' @return the distribution function
+#' @export
+#' @examples
+#' library(rfunctions)
+#' vals <- rnorm(100, mean = 4, sd = 2)
+#' boost.pnorm(vals, mean = 4, sd = 2, lower.tail = FALSE)
 boost.pnorm <- function(q, mean = 0, sd = 1, lower.tail = TRUE) {
-  .Call("ppnorm", q = q, mean = mean, sd = sd, lower.tail = lower.tail)
+  if (!(inherits(q, "numeric") | inherits(q, "integer"))) stop("Non-numeric argument to mathematical function")
+  .Call("ppnorm", q = as.numeric(q), mean = as.numeric(mean), 
+        sd = as.numeric(sd), lower.tail = lower.tail)
 }
