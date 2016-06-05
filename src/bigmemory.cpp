76403fecd5da92392531180e54620bd2f34b67a6
jaredhuling <huling@wisc.edu>
2016-06-04 19:38:51 -0500

    fixed memory issue related to matrix copying using map

--- src/bigmemory.cpp
+++ src/bigmemory.cpp
@@ -2,6 +2,7 @@
 #include "linop.h"
 #include "utils.h"
 #include "math.h"
+#include <bigmemory/MatrixAccessor.hpp>
 #include <bigmemory/BigMatrix.h>
 
 using Eigen::MatrixXf;
@@ -31,6 +32,17 @@ Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> BigEigenCrossprod(const Eigen::
   return retmat.template selfadjointView<Eigen::Upper>().rankUpdate( bigMat.adjoint() );
 }
 
+// Logic for BigColSums.
+template <typename T>
+NumericVector BigColSums(XPtr<BigMatrix> pMat, MatrixAccessor<T> mat) {
+  
+  // Create the vector we'll store the column sums in.
+  NumericVector colSums(pMat->ncol());
+  for (size_t i=0; i < pMat->ncol(); ++i)
+    colSums[i] = std::accumulate(mat[i], mat[i]+pMat->nrow(), 0.0);
+  return colSums;
+}
+
 RcppExport SEXP crossprod_big(SEXP X_)
 {
   using namespace Rcpp;
@@ -51,23 +63,27 @@ RcppExport SEXP crossprod_big(SEXP X_)
     
     if (type == 1) 
     {
-      MatrixXchar bM = Map<MatrixXchar>((char *)bMPtr->matrix(), bMPtr->nrow(), bMPtr->ncol()  );
-      MatrixXchar crossprod = BigEigenCrossprod(bM);
+      Map<MatrixXchar> bM = Map<MatrixXchar>((char *)bMPtr->matrix(), bMPtr->nrow(), bMPtr->ncol()  );
+      int p = bM.cols();
+      MatrixXchar crossprod = MatrixXchar(p, p).setZero().selfadjointView<Eigen::Upper>().rankUpdate( bM.adjoint() );
       return wrap(crossprod);
     } else if (type == 2) 
     {
-      MatrixXshort bM = Map<MatrixXshort>((short *)bMPtr->matrix(), bMPtr->nrow(), bMPtr->ncol()  );
-      MatrixXshort crossprod = BigEigenCrossprod(bM);
+      Map<MatrixXshort> bM = Map<MatrixXshort>((short *)bMPtr->matrix(), bMPtr->nrow(), bMPtr->ncol()  );
+      int p = bM.cols();
+      MatrixXshort crossprod = MatrixXshort(p, p).setZero().selfadjointView<Eigen::Upper>().rankUpdate( bM.adjoint() );
       return wrap(crossprod);
     } else if (type == 4) 
     {
-      MatrixXi bM = Map<MatrixXi>((int *)bMPtr->matrix(), bMPtr->nrow(), bMPtr->ncol()  );
-      MatrixXi crossprod = BigEigenCrossprod(bM);
+      Map<MatrixXi> bM = Map<MatrixXi>((int *)bMPtr->matrix(), bMPtr->nrow(), bMPtr->ncol()  );
+      int p = bM.cols();
+      MatrixXi crossprod = MatrixXi(p, p).setZero().selfadjointView<Eigen::Upper>().rankUpdate( bM.adjoint() );
       return wrap(crossprod);
     } else if (type == 8) 
     {
-      MatrixXd bM = Map<MatrixXd>((double *)bMPtr->matrix(), bMPtr->nrow(), bMPtr->ncol()  );
-      MatrixXd crossprod = BigEigenCrossprod(bM);
+      Map<MatrixXd> bM = Map<MatrixXd>((double *)bMPtr->matrix(), bMPtr->nrow(), bMPtr->ncol()  );
+      int p = bM.cols();
+      MatrixXd crossprod = MatrixXd(p, p).setZero().selfadjointView<Eigen::Upper>().rankUpdate( bM.adjoint() );
       return wrap(crossprod);
     } else {
       // We should never get here, but it resolves compiler warnings.
@@ -104,23 +120,23 @@ RcppExport SEXP colsums_big(SEXP X_)
     // 4, or 8 bytes. See the "type" argument in `?big.matrix`.
     if (type == 1) 
     {
-      MatrixXchar bM = Map<MatrixXchar>((char *)xpMat->matrix(), xpMat->nrow(), xpMat->ncol()  );
-      Vectorchar colSums = BigEigenColSums(bM);
+      Map<MatrixXchar> bM = Map<MatrixXchar>((char *)xpMat->matrix(), xpMat->nrow(), xpMat->ncol()  );
+      Vectorchar colSums = bM.colwise().sum();
       return wrap(colSums);
     } else if (type == 2) 
     {
-      MatrixXshort bM = Map<MatrixXshort>((short *)xpMat->matrix(), xpMat->nrow(), xpMat->ncol()  );
-      Vectorshort colSums = BigEigenColSums(bM);
+      Map<MatrixXshort> bM = Map<MatrixXshort>((short *)xpMat->matrix(), xpMat->nrow(), xpMat->ncol()  );
+      Vectorshort colSums = bM.colwise().sum();
       return wrap(colSums);
     } else if (type == 4) 
     {
-      MatrixXi bM = Map<MatrixXi>((int *)xpMat->matrix(), xpMat->nrow(), xpMat->ncol()  );
-      VectorXi colSums = BigEigenColSums(bM);
+      Map<MatrixXi> bM = Map<MatrixXi>((int *)xpMat->matrix(), xpMat->nrow(), xpMat->ncol()  );
+      VectorXi colSums = bM.colwise().sum();
       return wrap(colSums);
     } else if (type == 8) 
     {
-      MatrixXd bM = Map<MatrixXd>((double *)xpMat->matrix(), xpMat->nrow(), xpMat->ncol()  );
-      VectorXd colSums = BigEigenColSums(bM);
+      Map<MatrixXd> bM = Map<MatrixXd>((double *)xpMat->matrix(), xpMat->nrow(), xpMat->ncol()  );
+      VectorXd colSums = bM.colwise().sum();
       return wrap(colSums);
     } else {
       // We should never get here, but it resolves compiler warnings.
@@ -134,3 +150,38 @@ RcppExport SEXP colsums_big(SEXP X_)
   }
   return R_NilValue; //-Wall
 }
+
+
+
+// [[Rcpp::export]]
+NumericVector BigColSums(SEXP pBigMat) {
+  // First we have to tell Rcpp what class to use for big.matrix objects.
+  // This object stores the attributes of the big.matrix object passed to it
+  // by R.
+  XPtr<BigMatrix> xpMat(pBigMat);
+  
+  // To access values in the big.matrix, we need to create a MatrixAccessor
+  // object of the appropriate type. Note that in every case we are still
+  // returning a NumericVector: this is because big.matrix objects only store
+  // numeric values in R, even if their type is set to 'char'. The types
+  // simply correspond to the number of bytes used for each element.
+  switch(xpMat->matrix_type()) {
+  case 1:
+    return BigColSums(xpMat, MatrixAccessor<char>(*xpMat));
+  case 2:
+    return BigColSums(xpMat, MatrixAccessor<short>(*xpMat));
+  case 4:
+    return BigColSums(xpMat, MatrixAccessor<int>(*xpMat));
+  case 8:
+    return BigColSums(xpMat, MatrixAccessor<double>(*xpMat));
+  default:
+    // This case should never be encountered unless the implementation of
+    // big.matrix changes, but is necessary to implement shut up compiler
+    // warnings.
+    throw Rcpp::exception("unknown type detected for big.matrix object!");
+  }
+}
+
+
+
+
